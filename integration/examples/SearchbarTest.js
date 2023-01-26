2035a638a7e079a039851a088c46cd81bdebaabb
iwonafijal <fijal.iwona@gmail.com>
2023-01-26 22:48:34 +0100

    increase timeout duration

--- integration/examples/SearchbarTest.js
+++ integration/examples/SearchbarTest.js
@@ -5,7 +5,7 @@ describe("My First Test Suite", () => {
     // test case
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
-    cy.wait(2000);
+    cy.wait(5000);
     cy.get(".product").should("have.length", 5);
     cy.get(".product:visible").should("have.length", 4);
     // Parent child chaining
